6907749d692c1858e1626546a3144879af8758c7
FahadZaheerfzr <fahadzaheer19@gmail.com>
2022-12-29 00:16:01 +0500

    Favico updated, document title added

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import HomeLayout from '../components/HomeLayout'
 
 export default function LandingPage() {
+
+  useEffect(() => {
+    document.title = 'Roburna Info'
+  }, [])
 
   return (
     <div className='main-div'>
